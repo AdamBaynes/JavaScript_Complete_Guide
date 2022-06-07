5fa971dd17cb47c96aef6c6f4b82d96b03557889
Adam Baynes <adambaynes@Adams-Air.lan>
2022-06-07 11:53:34 -0400

    More work on calculator script

--- Section02/Section02_Lessons/basics-01-starting-project/assets/scripts/calculator.js
+++ Section02/Section02_Lessons/basics-01-starting-project/assets/scripts/calculator.js
@@ -1,2 +1,9 @@
 let result = 0;
+const defaultResult = 10;
+result = defaultResult + 10;
+let errorMessage = "An error \n" + "occurred!";
+
+let calculatorDescription = `${result} + 0`
+
+outputResult(result, calculatorDescription);
 
