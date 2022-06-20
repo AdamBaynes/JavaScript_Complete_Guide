4662327dd1b4b421be88922f2035213f4c31b330
Adam Baynes <adambaynes@Adams-Air.lan>
2022-06-19 21:19:08 -0400

    Continued work on the calculator

--- Section02/Section02_Lessons/basics-01-starting-project/assets/scripts/calculator.js
+++ Section02/Section02_Lessons/basics-01-starting-project/assets/scripts/calculator.js
@@ -1,9 +1,15 @@
-let result = 0;
+let result;
 const defaultResult = 10;
-result = defaultResult + 10;
-let errorMessage = "An error \n" + "occurred!";
+currentResult = defaultResult + 10;
+let num1 = 0;
+let num2 = 0;
 
-let calculatorDescription = `${result} + 0`
+function add(number1, number2){
+    result = number1 + number2; 
+    return result;
+}
+//This function is adding parameters number1 and number2 and then returning the result to be displayed
+const additionResult = add(0, 2);
 
-outputResult(result, calculatorDescription);
+outputResult(result, "0");
 
